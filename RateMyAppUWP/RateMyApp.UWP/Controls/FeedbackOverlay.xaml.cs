f5c575de4a414e3f9565eb45a9a9ba0d96cd2050
Edi Wang <Edi.Wang@outlook.com>
2016-02-21 16:39:14 +0800

    migrate control UI

--- RateMyAppUWP/RateMyApp.UWP/Controls/FeedbackOverlay.xaml.cs
+++ RateMyAppUWP/RateMyApp.UWP/Controls/FeedbackOverlay.xaml.cs
@@ -1,24 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+using RateMyApp.UWP.Helpers;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+using Windows.ApplicationModel.Email;
+using System.IO;
+using Windows.Storage;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+using Windows.ApplicationModel.Resources;
+using DoubleAnimation = Windows.UI.Xaml.Media.Animation.DoubleAnimation;
+using Colors = Windows.UI.Colors;
 
 namespace RateMyApp.UWP.Controls
 {
     public sealed partial class FeedbackOverlay : UserControl
     {
+        public static readonly DependencyProperty VisibilityForDesignProperty =
+            DependencyProperty.Register("VisibilityForDesign", typeof(Visibility), typeof(FeedbackOverlay), new PropertyMetadata(Visibility.Collapsed, null));
+
+        public static void SetVisibilityForDesign(FeedbackOverlay element, Visibility value)
+        {
+            element.SetValue(VisibilityForDesignProperty, value);
+        }
+
+        public static Visibility GetVisibilityForDesign(FeedbackOverlay element)
+        {
+            return (Visibility)element.GetValue(VisibilityForDesignProperty);
+        }
+
         public FeedbackOverlay()
         {
             this.InitializeComponent();
