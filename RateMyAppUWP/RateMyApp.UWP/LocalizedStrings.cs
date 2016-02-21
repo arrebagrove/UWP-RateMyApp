9e310ed20011971ea08dad322774ce34ac675175
Edi Wang <Edi.Wang@outlook.com>
2016-02-21 16:27:54 +0800

    migrate resources

---
+++ RateMyAppUWP/RateMyApp.UWP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using RateMyApp.UWP.Resources;
+
+namespace RateMyApp.UWP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources => _localizedResources;
+    }
+}
