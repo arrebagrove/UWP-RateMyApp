262263b0cb60470939d3977b0cf098dc55a151d7
Edi Wang <Edi.Wang@outlook.com>
2016-02-21 16:33:36 +0800

    update to usercontrol

--- RateMyAppUWP/RateMyApp.UWP/Controls/FeedbackOverlay.xaml.cs
+++ RateMyAppUWP/RateMyApp.UWP/Controls/FeedbackOverlay.xaml.cs
@@ -13,14 +13,11 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace RateMyApp.UWP.Controls
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class FeedbackOverlay : Page
+    public sealed partial class FeedbackOverlay : UserControl
     {
         public FeedbackOverlay()
         {
